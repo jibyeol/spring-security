1fbf9dcff22f593d9075ffb73668624bedd4c373
jibyeol <cjfrtt1234@naver.com>
2017-05-22 15:19:29 +0900

    spring security
    진행중

---
+++ src/main/java/com/gstar/security/StudySecurityApplication.java
@@ -0,0 +1,12 @@
+package com.gstar.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySecurityApplication.class, args);
+	}
+}
